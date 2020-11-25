48d5cf85a7b8b634e2819b8ca877502abb4f2a81
mkery <mkery@cs.cmu.edu>
2020-11-25 13:30:57 -0500

    first commit :tada:

---
+++ academi-yeah-ok/src/Card.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactCardFlip from "react-card-flip";
+
+class Card extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isFlipped: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }));
+  }
+
+  render() {
+    return (
+      <ReactCardFlip
+        isFlipped={this.state.isFlipped}
+        flipDirection="horizontal"
+      >
+        <div className="Card back" onClick={this.handleClick}>
+          <h1>Game</h1>
+        </div>
+
+        <div className="Card front" onClick={this.handleClick}>
+          <h1>Sample Card</h1>
+          <p>
+            A situation happens that is hard for everyone to deal with because
+            it's pretty terrible and toxic. Help.
+          </p>
+          <h3>How would you like to see this case solved by the department?</h3>
+        </div>
+      </ReactCardFlip>
+    );
+  }
+}
+
+export default Card;
---
+++ academi-yeah-ok/src/Deck.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from "./Card"
+
+class Deck extends React.Component {
+  render() {
+    return (
+      <Card/>
+    );
+  }
+}
+
+export default Deck;
